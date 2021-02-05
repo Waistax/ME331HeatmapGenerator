d1093604ed6ec5224f012f995c736d16bf570a61
Cem GEÇGEL <aa-albara@hotmail.com>
2021-02-05 08:38:00 +0300

    Drawing
    Input added.

--- kod/başaşağıderebeyi/heatmapgenerator/HeatmapGenerator.java
+++ kod/başaşağıderebeyi/heatmapgenerator/HeatmapGenerator.java
@@ -13,10 +13,14 @@ import java.util.*;
 import javax.swing.*;
 
 /** Generates a heatmap from a file. */
-public class HeatmapGenerator {
+public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelListener, MouseMotionListener {
+	private static final int[] midColor = new int[] { 255, 255, 0 };
+	private static final int[] minColor = new int[] { 0, 0, 200 };
+	private static final int[] maxColor = new int[] { 200, 0, 0 };
+	
 	/** Start of the program. */
-	public static void main(String[] args) {
-		HeatmapGenerator engine = new HeatmapGenerator();
+	public static void main(final String[] args) {
+		final HeatmapGenerator engine = new HeatmapGenerator();
 		engine.start();
 	}
 	
@@ -31,53 +35,66 @@ public class HeatmapGenerator {
 	/** The drawing tool. */
 	private final Graphics2D graphics;
 	
-	private int rowCount;
-	private int dataCount;
-	private int[] temperatures;
+	private final int rowCount;
+	private final int dataCount;
+	private final float[] temperatures;
+	private final Color[] colors;
+	private float xPos = 10.0F;
+	private float yPos = 10.0F;
+	private float scale = 30.0F;
+	private boolean dragged;
+	private int cursorX;
+	private int cursorY;
 	
 	/** Creates the engine. */
-	public HeatmapGenerator() {
-		this.frame = new JFrame("ME331 Heatmap Generator");
-		this.canvas = new Canvas();
+	private HeatmapGenerator() {
+		frame = new JFrame("ME331 Heatmap Generator");
+		canvas = new Canvas();
 		final Dimension dimension = new Dimension(1280, 720);
-		this.canvas.setMaximumSize(dimension);
-		this.canvas.setMinimumSize(dimension);
-		this.canvas.setPreferredSize(dimension);
-		this.frame.add(this.canvas);
-		this.frame.pack();
-		this.frame.setResizable(false);
-		this.frame.setLocationRelativeTo(null);
-		this.frame.setVisible(true);
-		this.frame.addWindowListener(new WindowAdapter() {
+		canvas.setMaximumSize(dimension);
+		canvas.setMinimumSize(dimension);
+		canvas.setPreferredSize(dimension);
+		frame.add(canvas);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(final WindowEvent e) {
 				HeatmapGenerator.this.stop();
 			}
 		});
-		this.frame.requestFocus();
-		this.canvas.requestFocus();
-		this.canvas.setBackground(Color.BLACK);
-		this.canvas.createBufferStrategy(2);
-		this.bufferStrategy = this.canvas.getBufferStrategy();
-		this.graphics = (Graphics2D)this.bufferStrategy.getDrawGraphics();
-		this.graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		this.graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		
-		Random random = new Random();
-		rowCount = random.nextInt() % 10+5;
-		dataCount = random.nextInt() % 15 + 10;
-		temperatures = new int[rowCount*dataCount];
-		for (int i = 0; i < temperatures.length; i++)
-			temperatures[i] = random.nextInt();
+		frame.requestFocus();
+		canvas.requestFocus();
+		canvas.setBackground(Color.BLACK);
+		canvas.createBufferStrategy(2);
+		canvas.addKeyListener(this);
+		canvas.addMouseListener(this);
+		canvas.addMouseMotionListener(this);
+		canvas.addMouseWheelListener(this);
+		bufferStrategy = canvas.getBufferStrategy();
+		graphics = (Graphics2D)bufferStrategy.getDrawGraphics();
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		final Random random = new Random();
+		rowCount = 10;
+		dataCount = 10;
+		temperatures = new float[rowCount * dataCount];
+		for (int i = 0; i < temperatures.length; i++) {
+			temperatures[i] = random.nextFloat();
+		}
+		colors = new Color[temperatures.length];
+		generateColors();
 	}
 	
 	/** Starts the engine. */
-	public void start() {
+	private void start() {
 		float previousTime = System.nanoTime();
 		float secondsCounter = 0.0F;
 		int ticks = 0;
-		this.running = true;
-		while (this.running) {
+		running = true;
+		while (running) {
 			float elapsedTime = System.nanoTime() - previousTime;
 			previousTime += elapsedTime;
 			elapsedTime /= 1000000000.0F;
@@ -93,19 +110,144 @@ public class HeatmapGenerator {
 			}
 			ticks++;
 			update();
-			this.bufferStrategy.show();
-			this.graphics.clearRect(0, 0, 1280, 720);
+			bufferStrategy.show();
+			graphics.clearRect(0, 0, 1280, 720);
 		}
 		System.exit(0);
 	}
 	
 	/** Stops the engine. */
-	public void stop() {
-		this.running = false;
+	private void stop() {
+		running = false;
 	}
 	
 	/** Updates the engine. */
-	public void update() {
-		
+	private void update() {
+		final int x = Math.round(xPos);
+		final int y = Math.round(yPos);
+		final int s = Math.round(scale);
+		for (int i = 0; i < rowCount; i++) {
+			for (int j = 0; j < dataCount; j++) {
+				final int index = j + i * dataCount;
+				graphics.setColor(colors[index]);
+				graphics.fillRect(x + i * s, y + j * s, s, s);
+			}
+		}
+		graphics.setColor(Color.WHITE);
+		for (int i = 0; i <= rowCount; i++) {
+			graphics.drawLine(x + i * s, y, x + i * s, y + dataCount * s);
+		}
+		for (int j = 0; j <= dataCount; j++) {
+			graphics.drawLine(x, y + j * s, x + rowCount * s, y + j * s);
+		}
+	}
+	
+	private void generateColors() {
+		float total = 0.0F;
+		float min = 0.0F;
+		float max = 0.0F;
+		for (final float temp : temperatures) {
+			total += temp;
+			min = Math.min(temp, min);
+			max = Math.max(temp, max);
+		}
+		final float mid = total / temperatures.length;
+		final int[] rgb = new int[3];
+		final float upper = max - mid;
+		final float lower = mid - min;
+		for (int i = 0; i < temperatures.length; i++) {
+			final float temp = temperatures[i];
+			final float d = temp - mid;
+			if (d < 0.0F) {
+				final float avg = (float)Math.sqrt(-d / lower);
+				final float inv = 1 - avg;
+				for (int j = 0; j < 3; j++) {
+					rgb[j] = Math.max(Math.min(
+							Math.round(HeatmapGenerator.midColor[j] * avg + HeatmapGenerator.minColor[j] * inv), 255),
+							0);
+				}
+			} else {
+				final float avg = (float)Math.sqrt(d / upper);
+				final float inv = 1 - avg;
+				for (int j = 0; j < 3; j++) {
+					rgb[j] = Math.max(Math.min(
+							Math.round(HeatmapGenerator.midColor[j] * avg + HeatmapGenerator.maxColor[j] * inv), 255),
+							0);
+				}
+			}
+			colors[i] = new Color(rgb[0], rgb[1], rgb[2]);
+		}
+	}
+	
+	@Override
+	public void keyTyped(final KeyEvent e) {
+	}
+	
+	@Override
+	public void keyPressed(final KeyEvent e) {
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_Y:
+			final Random random = new Random();
+			for (int i = 0; i < temperatures.length; i++) {
+				temperatures[i] = random.nextFloat();
+			}
+			generateColors();
+			return;
+		}
+	}
+	
+	@Override
+	public void keyReleased(final KeyEvent e) {
+	}
+	
+	@Override
+	public void mouseClicked(final MouseEvent e) {
+	}
+	
+	@Override
+	public void mousePressed(final MouseEvent e) {
+		switch (e.getButton()) {
+		case MouseEvent.BUTTON2:
+			dragged = true;
+			return;
+		}
+	}
+	
+	@Override
+	public void mouseReleased(final MouseEvent e) {
+		switch (e.getButton()) {
+		case MouseEvent.BUTTON2:
+			dragged = false;
+			return;
+		}
+	}
+	
+	@Override
+	public void mouseEntered(final MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseExited(final MouseEvent e) {
+	}
+	
+	@Override
+	public void mouseWheelMoved(final MouseWheelEvent e) {
+		scale += (float)e.getPreciseWheelRotation();
+	}
+	
+	@Override
+	public void mouseDragged(final MouseEvent e) {
+		if (dragged) {
+			xPos += e.getX() - cursorX;
+			yPos += e.getY() - cursorY;
+		}
+		cursorX = e.getX();
+		cursorY = e.getY();
+	}
+	
+	@Override
+	public void mouseMoved(final MouseEvent e) {
+		cursorX = e.getX();
+		cursorY = e.getY();
 	}
 }
