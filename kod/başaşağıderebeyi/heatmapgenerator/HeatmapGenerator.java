dc87208465d4e20b1cd4dd5897a6a4bc67e6cdd2
Cem GEÇGEL <aa-albara@hotmail.com>
2021-02-14 10:48:45 +0300

    Fixed reading
    Fixed the error on the y values of the reading. Added different temperature ranges that are independent.

--- kod/başaşağıderebeyi/heatmapgenerator/HeatmapGenerator.java
+++ kod/başaşağıderebeyi/heatmapgenerator/HeatmapGenerator.java
@@ -1,11 +1,12 @@
 /**
  * başaşağıderebeyi.heatmapgenerator.HeatmapGenerator.java
- * 0.3 / 2 Şub 2021 / 19:27:44
+ * 0.5 / 2 Şub 2021 / 19:27:44
  * Cem GEÇGEL (BaşAşağıDerebeyi)
  */
 package başaşağıderebeyi.heatmapgenerator;
 
 import java.awt.*;
+import java.awt.color.*;
 import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
@@ -15,11 +16,7 @@ import java.util.List;
 import javax.swing.*;
 
 /** Generates a heatmap from a file. */
-public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelListener, MouseMotionListener {
-	private static final int[] midColor = new int[] { 0, 100, 0 };
-	private static final int[] minColor = new int[] { 0, 0, 0 };
-	private static final int[] maxColor = new int[] { 255, 0, 0 };
-	
+public class HeatmapGenerator implements MouseWheelListener {
 	/** Start of the program. */
 	public static void main(final String[] args) {
 		try {
@@ -42,18 +39,20 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 	private final BufferStrategy bufferStrategy;
 	/** The drawing tool. */
 	private final Graphics2D graphics;
-	
+	/** The number of waterways in the field. */
 	private int rowCount;
+	/** The number of measurements for each waterway. */
 	private int dataCount;
+	/** The temperature data. */
 	private float[] temperatures;
+	/** The color for each temperature. */
 	private final Color[] colors;
-	private float xPos = 10.0F;
-	private float yPos = 10.0F;
-	private float scale = 15.0F;
+	/** The color for the text temperature value for each temperature. */
+	private final Color[] textColors;
+	/** The scale to draw the tiles. */
+	private float scale = 26.0F;
+	/** The height to width ratio of the tiles. */
 	private float ratio;
-	private boolean dragged;
-	private int cursorX;
-	private int cursorY;
 	
 	/** Creates the engine. */
 	private HeatmapGenerator() {
@@ -78,9 +77,6 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 		canvas.requestFocus();
 		canvas.setBackground(Color.BLACK);
 		canvas.createBufferStrategy(2);
-		canvas.addKeyListener(this);
-		canvas.addMouseListener(this);
-		canvas.addMouseMotionListener(this);
 		canvas.addMouseWheelListener(this);
 		bufferStrategy = canvas.getBufferStrategy();
 		graphics = (Graphics2D)bufferStrategy.getDrawGraphics();
@@ -88,6 +84,7 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 		readData();
 		colors = new Color[temperatures.length];
+		textColors = new Color[temperatures.length];
 		generateColors();
 	}
 	
@@ -95,15 +92,12 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 	private void start() {
 		float previousTime = System.nanoTime();
 		float secondsCounter = 0.0F;
-		int ticks = 0;
 		running = true;
 		while (running) {
 			float elapsedTime = System.nanoTime() - previousTime;
 			previousTime += elapsedTime;
 			elapsedTime /= 1000000000.0F;
 			if ((secondsCounter += elapsedTime) >= 1.0F) {
-				System.out.println("Tick Rate: " + ticks);
-				ticks = 0;
 				secondsCounter--;
 			}
 			try {
@@ -111,10 +105,14 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 			} catch (final InterruptedException e1) {
 				e1.printStackTrace();
 			}
-			ticks++;
 			update();
 			bufferStrategy.show();
 			graphics.clearRect(0, 0, 1280, 720);
+			try {
+				Thread.sleep(10);
+			} catch (final InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		System.exit(0);
 	}
@@ -126,21 +124,21 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 	
 	/** Updates the engine. */
 	private void update() {
-		final int x = Math.round(xPos);
-		final int y = Math.round(yPos);
-		float pixelScale = (float)Math.pow(1.2, scale);
+		final float pixelScale = (float)Math.pow(1.2, scale);
 		final int sx = Math.round(pixelScale);
 		final int sy = Math.round(pixelScale * ratio);
+		final int x = (1280 - sx * rowCount) / 2;
+		final int y = (720 - sy * dataCount) / 2;
 		graphics.setFont(new Font("Consolas", Font.PLAIN, sx / 8));
 		for (int i = 0; i < rowCount; i++) {
 			for (int j = 0; j < dataCount; j++) {
 				final int index = j + i * dataCount;
 				final int xpos = x + i * sx;
-				final int ypos = y + (dataCount-j-1) * sy;
+				final int ypos = y + (dataCount - j - 1) * sy;
 				graphics.setColor(colors[index]);
 				graphics.fillRect(xpos, ypos, sx, sy);
-				graphics.setColor(Color.WHITE);
-				graphics.drawString(""+temperatures[index], xpos + 1, ypos + sy - 1);
+				graphics.setColor(textColors[index]);
+				graphics.drawString("" + temperatures[index], xpos + 3, ypos + sy - 3);
 			}
 		}
 		graphics.setColor(Color.WHITE);
@@ -152,48 +150,52 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 		}
 	}
 	
+	/** Generates the colors from the temperature data. */
 	private void generateColors() {
-		float total = 0.0F;
-		float min = 0.0F;
-		float max = 0.0F;
-		for (final float temp : temperatures) {
-			total += temp;
-			min = Math.min(temp, min);
-			max = Math.max(temp, max);
-		}
-		final float mid = total / temperatures.length;
-		float l = max - min;
-		final int[] rgb = new int[3];
-		final float upper = max - mid;
-		final float lower = mid - min;
+		final float[] tempRanges = new float[] { 15.0F, 25.0F, 30.0F, 40.0F };
+		final ColorSpace colorSpace = ColorSpace.getInstance(ColorSpace.CS_LINEAR_RGB);
+		final float[][] colorRanges = new float[][] { colorSpace.fromRGB(new float[] { 0.0F, 0.0F, 0.0F }),
+				colorSpace.fromRGB(new float[] { 0.0F, 0.0F, 1.0F }),
+				colorSpace.fromRGB(new float[] { 1.0F, 1.0F, 0.0F }),
+				colorSpace.fromRGB(new float[] { 1.0F, 0.1F, 0.0F }),
+				colorSpace.fromRGB(new float[] { 1.0F, 0.1F, 0.0F }),
+				colorSpace.fromRGB(new float[] { 1.0F, 0.0F, 0.0F }) };
+		final float[] interpolated = new float[colorSpace.getNumComponents()];
 		for (int i = 0; i < temperatures.length; i++) {
-			final float temp = temperatures[i];
-			final float avg = (temp-min) /l;
-			final float inv = 1 - avg;
-			for (int j = 0; j < 3; j++) {
-				rgb[j] = (int)Math.max(Math.min(
-						Math.round(HeatmapGenerator.minColor[j] * inv + HeatmapGenerator.maxColor[j] * avg), 255),
-						0);
+			int lowerRange = 0;
+			for (int j = 0; j < tempRanges.length - 1; j++) {
+				if (temperatures[i] > tempRanges[j] && tempRanges[j] > tempRanges[lowerRange]) {
+					lowerRange = j;
+				}
 			}
-			colors[i] = new Color(rgb[0], rgb[1], rgb[2]);
+			final float d = Math.max(Math.min(
+					(temperatures[i] - tempRanges[lowerRange]) / (tempRanges[lowerRange + 1] - tempRanges[lowerRange]),
+					1.0F), 0.0F);
+			final float inv = 1.0F - d;
+			for (int j = 0; j < interpolated.length; j++) {
+				interpolated[j] = colorRanges[lowerRange * 2][j] * inv + colorRanges[lowerRange * 2 + 1][j] * d;
+			}
+			colors[i] = new Color(colorSpace, interpolated, 1.0F);
+			textColors[i] = colors[i].getRed() + colors[i].getGreen() + colors[i].getBlue() >= 255 ? Color.BLACK
+					: Color.WHITE;
 		}
 	}
 	
+	/** Reads the data in the output.txt file. */
 	private void readData() {
-		List<String> lines = new ArrayList<>();
+		final List<String> lines = new ArrayList<>();
 		final File file = new File("output.txt");
 		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
 				lines.add(line);
 			}
-		}
-		catch (Exception e) {
+		} catch (final Exception e) {
 			e.printStackTrace();
 		}
-		float rowLength = Float.parseFloat(lines.get(0).substring("Row Length: ".length()));
-		float stepSize = Float.parseFloat(lines.get(1).substring("Step Size: ".length()));
-		float rowWidth = Float.parseFloat(lines.get(2).substring("Row Width: ".length()));
+		final float rowLength = Float.parseFloat(lines.get(0).substring("Row Length: ".length()));
+		final float stepSize = Float.parseFloat(lines.get(1).substring("Step Size: ".length()));
+		final float rowWidth = Float.parseFloat(lines.get(2).substring("Row Width: ".length()));
 		rowCount = Integer.parseInt(lines.get(3).substring("Row Count: ".length()));
 		dataCount = (int)Math.floor(rowLength / stepSize);
 		ratio = stepSize / rowWidth;
@@ -201,81 +203,19 @@ public class HeatmapGenerator implements KeyListener, MouseListener, MouseWheelL
 		System.out.println("Step Size: " + stepSize);
 		System.out.println("Data Count: " + dataCount);
 		System.out.println("Row Width: " + rowWidth);
-		temperatures = new float[rowCount*dataCount];
+		temperatures = new float[rowCount * dataCount];
 		for (int i = 0; i < temperatures.length; i++) {
-			temperatures[i] = Float.parseFloat(lines.get(5+i).substring("Row: 0 | Position: 0.00 | Temperature: ".length()));
+			int x = i / dataCount;
+			int y = i % dataCount;
+			int realX = x;
+			int realY = x % 2 == 1 ? dataCount-1-y : y;
+			temperatures[realX * dataCount + realY] = Float
+					.parseFloat(lines.get(5 + i).substring("Row: 0 | Position: 0.00 | Temperature: ".length()));
 		}
-	}
-	
-	@Override
-	public void keyTyped(final KeyEvent e) {
-	}
-	
-	@Override
-	public void keyPressed(final KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_Y:
-			final Random random = new Random();
-			for (int i = 0; i < temperatures.length; i++) {
-				temperatures[i] = random.nextFloat();
-			}
-			generateColors();
-			return;
-		}
-	}
-	
-	@Override
-	public void keyReleased(final KeyEvent e) {
-	}
-	
-	@Override
-	public void mouseClicked(final MouseEvent e) {
-	}
-	
-	@Override
-	public void mousePressed(final MouseEvent e) {
-		switch (e.getButton()) {
-		case MouseEvent.BUTTON2:
-			dragged = true;
-			return;
-		}
-	}
-	
-	@Override
-	public void mouseReleased(final MouseEvent e) {
-		switch (e.getButton()) {
-		case MouseEvent.BUTTON2:
-			dragged = false;
-			return;
-		}
-	}
-	
-	@Override
-	public void mouseEntered(final MouseEvent e) {
-	}
-	
-	@Override
-	public void mouseExited(final MouseEvent e) {
 	}
 	
 	@Override
 	public void mouseWheelMoved(final MouseWheelEvent e) {
 		scale += (float)e.getPreciseWheelRotation();
-	}
-	
-	@Override
-	public void mouseDragged(final MouseEvent e) {
-		if (dragged) {
-			xPos += e.getX() - cursorX;
-			yPos += e.getY() - cursorY;
-		}
-		cursorX = e.getX();
-		cursorY = e.getY();
-	}
-	
-	@Override
-	public void mouseMoved(final MouseEvent e) {
-		cursorX = e.getX();
-		cursorY = e.getY();
 	}
 }
